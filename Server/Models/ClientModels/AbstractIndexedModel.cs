0dad2fa0ae47bad14f233f6e26b45e91fcca35cb
Austin Anderson <Austin.David.Anderson1@gmail.com>
2019-05-11 08:42:23 -0500

    still not sure because of assigning null

--- Server/Models/ClientModels/AbstractIndexedModel.cs
+++ Server/Models/ClientModels/AbstractIndexedModel.cs
@@ -3,24 +3,27 @@ using System.Collections.Generic;
 
 namespace Server.Models.ClientModels
 {
-    public abstract class AbstractIndexedModel<T>
+    public abstract class AbstractIndexedModel<TDictType,TReturnType>
     {
-        private Dictionary<int,(T,string)> indexedProps;
-        protected abstract Dictionary<int,(T,string)> GetIndexedProps();
-        public abstract int IndexLimit {get;}
-        private (T,string) GetOrThrow(int ndx){
+        private Dictionary<int,(TDictType,string)> indexedProps;
+        protected abstract Dictionary<int,(TDictType,string)> GetIndexedProps();
+        private (TDictType,string) GetOrThrow(int ndx){
             if(indexedProps==null) indexedProps=GetIndexedProps();
             if(ndx<0) throw new ArgumentOutOfRangeException("index must be positive");
-            if(indexedProps.TryGetValue(ndx,out (T,string) val)){
+            if(indexedProps.TryGetValue(ndx,out (TDictType,string) val)){
                 return val;
             }
             throw new ArgumentOutOfRangeException(
                 $"index must be less than {nameof(IndexLimit)} of {IndexLimit}"
             );
         }
+        protected abstract TReturnType ReturnFromDict(TDictType type);
+        public abstract int IndexLimit {get;}
         public string NameOf(int ndx)=>GetOrThrow(ndx).Item2;
-        public T this[int ndx] {
-            get
+        public TReturnType this[int ndx] {
+            get=>ReturnFromDict(GetOrThrow(ndx).Item1);
+            set{
+            }
         }
     }
 }
