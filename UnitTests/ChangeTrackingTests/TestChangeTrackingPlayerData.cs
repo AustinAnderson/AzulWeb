5a9a2098cc50324f5ee465515241586dbb0c9e8e
Austin Anderson <Austin.David.Anderson1@gmail.com>
2019-05-18 15:36:24 -0500

    tests all good

--- UnitTests/ChangeTrackingTests/TestChangeTrackingPlayerData.cs
+++ UnitTests/ChangeTrackingTests/TestChangeTrackingPlayerData.cs
@@ -30,7 +30,7 @@ namespace UnitTests.ChangeTrackingTests
             );
             if(floorChange==null) Assert.Fail($"expected id {floorChangeTileId} to be one in the change list");
             Assert.AreEqual(
-                $"{nameof(GameStateModel.PlayerData)}[0].{nameof(PlayerDataModel.FloorLine)}[0]",
+                $"{nameof(GameStateModel.PlayerData)}[0].{nameof(PlayerDataModel.FloorLine)}[1]",
                 floorChange.NewJsonPath
             );
             int patternChangeTileId=beforeState.SharedData.Factories[0].TileOne.Id;
@@ -68,7 +68,7 @@ namespace UnitTests.ChangeTrackingTests
             Assert.AreEqual( 0,patternLine1WallChange.PlayerIndex,"tile 1 player ndx" );
             Assert.AreEqual( patternLine1TileId,patternLine1WallChange.TileId,"tile 1 tileId" );
 
-            int patternLine5TileId=beforeState.PlayerData[0].PatternLines[4][2].Id;
+            int patternLine5TileId=beforeState.PlayerData[0].PatternLines[4][0].Id;
             var patternLine5WallChange=result.WallChanges.FirstOrDefault(x=>
                 x.TileId==patternLine5TileId
             );
