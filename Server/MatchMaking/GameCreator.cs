c5e5b93c8c571420e810c58bd79a4a84da3d7a80
Austin Anderson <Austin.David.Anderson1@gmail.com>
2019-05-26 13:09:10 -0500

    started to think about initial game setup

---
+++ Server/MatchMaking/GameCreator.cs
@@ -0,0 +1,13 @@
+using System;
+namespace Server.MatchMaking
+{
+    public class GameCreator
+    {
+        //host the game ie one player creates a game
+        //tie signlr hubs to room codes using persistance layer,
+        //then once all clients are connected, send the initial game state 
+        //and remove from the persistance store
+        //public HostGame
+    }
+}
+
