b415709a6baf418b86607a3b3b79596fc982c81a
Austin Anderson <Austin.David.Anderson1@gmail.com>
2019-06-02 15:56:04 -0500

    set to serve static html from /Client/dist build by parcel

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -42,6 +42,7 @@ namespace Server
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.UseStaticFiles("/Client/dist");
 
             app.UseHttpsRedirection();
             app.UseMvc();
