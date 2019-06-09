7a286df1dc8b581700edc02b3383acace1ac13db
Austin Anderson <Austin.David.Anderson1@gmail.com>
2019-06-08 19:32:41 -0500

    test

--- AzulWeb/Client/JavaScript/views/logon/logon-page.js
+++ AzulWeb/Client/JavaScript/views/logon/logon-page.js
@@ -7,40 +7,48 @@ export class LogonPage extends LitElement
     static TagName(){ return "longon-page"; }
     static get properties(){
         return {
-            _private: {type:Object},
+            this._private: {type:Object},
         };
+    }
+    /**
+     * intended to be overwritten with a call back for making the server cancel the game
+     * @param {string} cancelledGameId the id of the game that was cancelled
+     * 
+     * @memberof LogonButtonsAndInput
+     */
+    OnGameCancelled=(cancelledGameId) =>{
+        alert(LogonPage.name+"."+OnGameCancelled.name+" never assigned!");
+    }
+    /**
+     * intended to be overwritten with a call back for making the server start the game
+     * and the outer class to switch the view to the main game view
+     * 
+     * @memberof LogonButtonsAndInput
+     */
+    OnGameStart=()=>{
+        alert(LogonPage.name+"."+OnGameStart.name+" never assigned!");
+    }
+    /**
+     * intended to be overwritten with a call back for retrieving the new
+     * game code from the server
+     * @returns {string} server created game code
+     * @memberof LogonButtonsAndInput
+     */
+    OnHostGameClicked=()=>{
+        alert(LogonPage.name+"."+OnHostGameClicked.name+" never assigned!");
+        return "";
+    }
+    RoomCodeChanged(){
+        if(gameId==""){ this._private.RoomCodeClearedAction();}
+        else { this._private.RoomCodeEnteredAction(); }
+    }
+    UserNameChanged(){
+        if(userId==""){ this._private.UserNameClearedAction();}
+        else { this._private.UserNameEnteredAction(); }
     }
     constructor(){
         super();
-        console.log("logon constructor run");
-        /**
-         * intended to be overwritten with a call back for making the server cancel the game
-         * @param {string} cancelledGameId the id of the game that was cancelled
-         * 
-         * @memberof LogonButtonsAndInput
-         */
-        this.OnGameCancelled=(cancelledGameId) =>{
-            alert(LogonPage.name+"."+OnGameCancelled.name+" never assigned!");
-        }
-        /**
-         * intended to be overwritten with a call back for making the server start the game
-         * and the outer class to switch the view to the main game view
-         * 
-         * @memberof LogonButtonsAndInput
-         */
-        this.OnGameStart=()=>{
-            alert(LogonPage.name+"."+OnGameStart.name+" never assigned!");
-        }
-        /**
-         * intended to be overwritten with a call back for retrieving the new
-         * game code from the server
-         * @returns {string} server created game code
-         * @memberof LogonButtonsAndInput
-         */
-        this.OnHostGameClicked=()=>{
-            alert(LogonPage.name+"."+OnHostGameClicked.name+" never assigned!");
-            return "";
-        }
+        
         this._private={
             gameId: "",
             userId: "",
@@ -57,28 +65,20 @@ export class LogonPage extends LitElement
             RemoveAllUsers(){
 
             },
-            RoomCodeChanged(){
-                if(gameId==""){ _private.RoomCodeClearedAction();}
-                else { _private.RoomCodeEnteredAction(); }
-            },
-            UserNameChanged(){
-                if(userId==""){ _private.UserNameClearedAction();}
-                else { _private.UserNameEnteredAction(); }
-            },
             CancelGameButtonClicked(){
-                OnGameCancelled(_private.gameId);
+                OnGameCancelled(this._private.gameId);
                 RoomTornDown();
             },
             contextButtonHandlers:{
                 HostNewGameClicked(){
-                    _private.gameId=OnHostGameClicked();
-                    _private.become.WaitForMorePlayers();
+                    this._private.gameId=OnHostGameClicked();
+                    this._private.become.WaitForMorePlayers();
                 },
                 JoinGameClicked(){
-                    _private.become.WaitForHostToStart();
+                    this._private.become.WaitForHostToStart();
                 },
                 LeaveLobby(){
-                    _private.become.ReadyToJoin();
+                    this._private.become.ReadyToJoin();
                 },
                 StartGameClicked(){
                     OnGameStart();
@@ -86,91 +86,91 @@ export class LogonPage extends LitElement
             },
             become:{
                 NeedNameAndCode(){
-                    _private.cancelGameButtonHidden=true;
-                    _private.buttonContext="Host Game";
-                    _private.buttonDisabled=true;
-                    _private.gameIdEditable=true;
-                    _private.userIdEditable=true;
-                    _private.ContextAwareButtonAction=()=>{};
-                    _private.RoomCodeClearedAction=()=>{};
-                    _private.UserNameClearedAction=()=>{};
-                    _private.RoomCodeEnteredAction=_private.become.NeedNameToJoin;
-                    _private.UserNameEnteredAction=_private.become.ReadyToHost;
+                    this._private.cancelGameButtonHidden=true;
+                    this._private.buttonContext="Host Game";
+                    this._private.buttonDisabled=true;
+                    this._private.gameIdEditable=true;
+                    this._private.userIdEditable=true;
+                    this._private.ContextAwareButtonAction=()=>{};
+                    this._private.RoomCodeClearedAction=()=>{};
+                    this._private.UserNameClearedAction=()=>{};
+                    this._private.RoomCodeEnteredAction=this._private.become.NeedNameToJoin;
+                    this._private.UserNameEnteredAction=this._private.become.ReadyToHost;
                 },
                 NeedNameToJoin(){
-                    _private.cancelGameButtonHidden=true;
-                    _private.buttonContext="Join Game";
-                    _private.buttonDisabled=true;
-                    _private.ContextAwareButtonAction=()=>{};
-                    _private.gameIdEditable=true;
-                    _private.userIdEditable=true;
-                    _private.RoomCodeClearedAction=_private.become.NeedNameAndCode;
-                    _private.RoomCodeEnteredAction=()=>{};
-                    _private.UserNameClearedAction=()=>{};
-                    _private.UserNameEnteredAction=_private.become.ReadyToJoin;
+                    this._private.cancelGameButtonHidden=true;
+                    this._private.buttonContext="Join Game";
+                    this._private.buttonDisabled=true;
+                    this._private.ContextAwareButtonAction=()=>{};
+                    this._private.gameIdEditable=true;
+                    this._private.userIdEditable=true;
+                    this._private.RoomCodeClearedAction=this._private.become.NeedNameAndCode;
+                    this._private.RoomCodeEnteredAction=()=>{};
+                    this._private.UserNameClearedAction=()=>{};
+                    this._private.UserNameEnteredAction=this._private.become.ReadyToJoin;
                 },
                 ReadyToHost(){
-                    _private.cancelGameButtonHidden=true;
-                    _private.buttonContext="Host Game";
-                    _private.buttonDisabled=false;
-                    _private.ContextAwareButtonAction=_private.contextButtonHandlers.HostNewGameClicked;
-                    _private.gameIdEditable=true;
-                    _private.userIdEditable=true;
-                    _private.RoomCodeClearedAction=()=>{};
-                    _private.RoomCodeEnteredAction=_private.become.NeedNameToJoin;
-                    _private.UserNameClearedAction=_private.become.NeedNameAndCode;
-                    _private.UserNameEnteredAction=()=>{};
+                    this._private.cancelGameButtonHidden=true;
+                    this._private.buttonContext="Host Game";
+                    this._private.buttonDisabled=false;
+                    this._private.ContextAwareButtonAction=this._private.contextButtonHandlers.HostNewGameClicked;
+                    this._private.gameIdEditable=true;
+                    this._private.userIdEditable=true;
+                    this._private.RoomCodeClearedAction=()=>{};
+                    this._private.RoomCodeEnteredAction=this._private.become.NeedNameToJoin;
+                    this._private.UserNameClearedAction=this._private.become.NeedNameAndCode;
+                    this._private.UserNameEnteredAction=()=>{};
                 },
                 ReadyToJoin(){
-                    _private.cancelGameButtonHidden=true;
-                    _private.buttonContext="Join Game";
-                    _private.buttonDisabled=false;
-                    _private.ContextAwareButtonAction=_private.contextButtonHandlers.JoinGameClicked;
-                    _private.gameIdEditable=true;
-                    _private.userIdEditable=true;
-                    _private.RoomCodeClearedAction=_private.become.ReadyToHost;
-                    _private.RoomCodeEnteredAction=()=>{};
-                    _private.UserNameClearedAction=_private.become.NeedNameToJoin;
-                    _private.UserNameEnteredAction=()=>{};
+                    this._private.cancelGameButtonHidden=true;
+                    this._private.buttonContext="Join Game";
+                    this._private.buttonDisabled=false;
+                    this._private.ContextAwareButtonAction=this._private.contextButtonHandlers.JoinGameClicked;
+                    this._private.gameIdEditable=true;
+                    this._private.userIdEditable=true;
+                    this._private.RoomCodeClearedAction=this._private.become.ReadyToHost;
+                    this._private.RoomCodeEnteredAction=()=>{};
+                    this._private.UserNameClearedAction=this._private.become.NeedNameToJoin;
+                    this._private.UserNameEnteredAction=()=>{};
                 },
                 WaitForMorePlayers()
                 {
-                    _private.cancelGameButtonHidden=false;
-                    _private.buttonContext="Start Game";
-                    _private.buttonDisabled=true;
-                    _private.ContextAwareButtonAction=()=>{};
-                    _private.gameIdEditable=false;
-                    _private.userIdEditable=false;
-                    _private.RoomCodeClearedAction=()=>{};
-                    _private.RoomCodeEnteredAction=()=>{};
-                    _private.UserNameClearedAction=()=>{};
-                    _private.UserNameEnteredAction=()=>{};
+                    this._private.cancelGameButtonHidden=false;
+                    this._private.buttonContext="Start Game";
+                    this._private.buttonDisabled=true;
+                    this._private.ContextAwareButtonAction=()=>{};
+                    this._private.gameIdEditable=false;
+                    this._private.userIdEditable=false;
+                    this._private.RoomCodeClearedAction=()=>{};
+                    this._private.RoomCodeEnteredAction=()=>{};
+                    this._private.UserNameClearedAction=()=>{};
+                    this._private.UserNameEnteredAction=()=>{};
                 },
                 ReadyToStart()
                 {
-                    _private.cancelGameButtonHidden=false;
-                    _private.buttonContext="Start Game";
-                    _private.buttonDisabled=false;
-                    _private.ContextAwareButtonAction=_private.contextButtonHandlers.StartGameClicked;
-                    _private.gameIdEditable=false;
-                    _private.userIdEditable=false;
-                    _private.RoomCodeClearedAction=()=>{};
-                    _private.RoomCodeEnteredAction=()=>{};
-                    _private.UserNameClearedAction=()=>{};
-                    _private.UserNameEnteredAction=()=>{};
+                    this._private.cancelGameButtonHidden=false;
+                    this._private.buttonContext="Start Game";
+                    this._private.buttonDisabled=false;
+                    this._private.ContextAwareButtonAction=this._private.contextButtonHandlers.StartGameClicked;
+                    this._private.gameIdEditable=false;
+                    this._private.userIdEditable=false;
+                    this._private.RoomCodeClearedAction=()=>{};
+                    this._private.RoomCodeEnteredAction=()=>{};
+                    this._private.UserNameClearedAction=()=>{};
+                    this._private.UserNameEnteredAction=()=>{};
                 },
                 WaitForHostToStart()
                 {
-                    _private.cancelGameButtonHidden=true;
-                    _private.buttonContext="Leave Lobby";
-                    _private.buttonDisabled=false;
-                    _private.ContextAwareButtonAction=_private.contextButtonHandlers.LeaveLobby;
-                    _private.gameIdEditable=false;
-                    _private.userIdEditable=false;
-                    _private.RoomCodeClearedAction=()=>{};
-                    _private.RoomCodeEnteredAction=()=>{};
-                    _private.UserNameClearedAction=()=>{};
-                    _private.UserNameEnteredAction=()=>{};
+                    this._private.cancelGameButtonHidden=true;
+                    this._private.buttonContext="Leave Lobby";
+                    this._private.buttonDisabled=false;
+                    this._private.ContextAwareButtonAction=this._private.contextButtonHandlers.LeaveLobby;
+                    this._private.gameIdEditable=false;
+                    this._private.userIdEditable=false;
+                    this._private.RoomCodeClearedAction=()=>{};
+                    this._private.RoomCodeEnteredAction=()=>{};
+                    this._private.UserNameClearedAction=()=>{};
+                    this._private.UserNameEnteredAction=()=>{};
                 }
             }
         }
@@ -180,7 +180,7 @@ export class LogonPage extends LitElement
     {
         document.querySelector("div")
         if(playerCount>=2){
-            _private.become.ReadyToStart();
+            this._private.become.ReadyToStart();
         }
     }
     UserRemoved(userId){
@@ -191,9 +191,9 @@ export class LogonPage extends LitElement
      * @memberof LogonButtonsAndInput
      */
     RoomTornDown(){
-        _private.RemoveAllUsers();
-        _private.gameId="";//will this fire onchange?
-        _private.become.ReadyToHost();
+        this._private.RemoveAllUsers();
+        this._private.gameId="";//will this fire onchange?
+        this._private.become.ReadyToHost();
     }
 
     
@@ -203,14 +203,14 @@ export class LogonPage extends LitElement
                 <papper-input label="room code" 
                               disabled="${this._private.gameIdEditable}"
                               value="${this._private.gameId}"
-                              @change="${this._private.RoomCodeChanged}"
+                              @change="${this.RoomCodeChanged}"
                               >
                 </papper-input>
 
                 <papper-input label="user name" 
                               disabled="${this._private.userIdEditable}"
                               value="${this._private.userId}"
-                              @change="${this._private.UserNameChanged}"
+                              @change="${this.UserNameChanged}"
                               >
                 </papper-input>
 
